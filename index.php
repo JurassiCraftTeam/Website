dafa2202a6381eea51b2b7ad4a8c4484a4af1c1b
SkyForge_ <goldr1000ftw@gmail.com>
2018-05-01 16:27:42 -0500

    Basic Setup and the start to a header. WIP
    Dont take anything you see as final, It's just on github because I want to be able to work on it wherever I am

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+    <meta charset="utf-8"></meta>
+      <!--Tab Title-->
+    <title>JurassiCraft</title>
+
+      <!--Meta-->
+    <meta content="Minecraft mod and community" name="description"></meta>
+    <meta content="SkyForge_" name="author"></meta>
+
+      <!--Favicon-->
+    <!--     <link href="images/Icons/favicon.ico" rel="shortcut icon"></link>     -->
+
+      <!--Fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet"></link>
+
+      <!--Styles-->
+    <link href="styles/main.css" rel="stylesheet"></link>
+    <link href="styles/fonts.css" rel="stylesheet"></link>
+
+      <!--Scripts-->
+    <script src="scripts/main.js"></script>
+  </head>
+
+  <body>
+    <?php include 'modules/navbar.html'?>
+  </body>
+</html>
