60d2f9106fe5e5b81d8ab38acac5f679b61381fc
SkyForge_ <goldr1000ftw@gmail.com>
2018-05-04 22:51:12 -0500

    New falling in the trees theme

--- index.php
+++ index.php
@@ -24,14 +24,28 @@
   </head>
 
   <body>
-    <?php include 'modules/navbar.html'?>
-    <br>
 
-    <div class="parallax" id="0.8">
+    <div class="splash" style="width:100%;background-color:white;height:700px;margin-bottom:400px;position:absolute;text-align:center;">
+      <img src="images/JC_Title_Red.png" style="margin-top:150px;">
+    </div>
+
+    <div class="parallax" id="0.3">
+      <img src="images/farthest.png" style="width: 100%;margin-top:500;">
+      <div style="width:100%;height:250px;background-color:#0f0f76;position:absolute;margin-top:640;z-index:3;"></div>
+    </div>
+    <div class="parallax" id="0.65">
+      <img src="images/middle.png" style="width: 100%; margin-top:500;">
+      <div style="width:100%;height:300px;background-color:#020232;position:absolute;margin-top:940;z-index:3;"></div>
+    </div>
+
+    <br><br><br><br><br><br><br><br><br>
+
+
+    <div class="parallax" id="0.8" style="margin-top: 41%;z-index:6;">
       <?php include 'modules/landingInfo.html' ?>
     </div>
     <div class="parallax" id="1.3">
-      <img src="images/BrachioFem.png" style="margin-left: 57%; margin-top: -30%;">
+      <img src="images/BrachioFem.png" style="margin-left: 57%;margin-top: -20%;">
     </div>
 
     <br><br><br>
