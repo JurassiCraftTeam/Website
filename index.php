871107cb95ac8597ffbc3826388a11eae2007393
SkyForge_ <goldr1000ftw@gmail.com>
2018-05-05 11:38:22 -0500

    Moved css and added full device support

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
       <!--Meta-->
     <meta content="Minecraft mod and community" name="description"></meta>
     <meta content="SkyForge_" name="author"></meta>
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no" />
 
       <!--Favicon-->
     <!--     <link href="images/Icons/favicon.ico" rel="shortcut icon"></link>     -->
@@ -25,31 +26,25 @@
 
   <body>
 
-    <div class="splash" style="width:100%;background-color:white;height:700px;margin-bottom:400px;position:absolute;text-align:center;">
-      <img src="images/JC_Title_Red.png" style="margin-top:150px;">
-    </div>
+    <div class="splash">
+      <img src="images/JC_Title_Red.png">
 
-    <div class="parallax" id="0.3">
-      <img src="images/farthest.png" style="width: 100%;margin-top:500;">
-      <div style="width:100%;height:250px;background-color:#0f0f76;position:absolute;margin-top:640;z-index:3;"></div>
-    </div>
-    <div class="parallax" id="0.65">
-      <img src="images/middle.png" style="width: 100%; margin-top:500;">
-      <div style="width:100%;height:300px;background-color:#020232;position:absolute;margin-top:940;z-index:3;"></div>
+      <div class="parallax" id="0.3">
+        <div class="layer2">
+          <img src="images/farthest.png">
+        </div>
+      </div>
+      <div class="parallax" id="0.65">
+        <div class="layer3">
+          <img src="images/middle.png">
+        </div>
+      </div>
     </div>
 
     <br><br><br><br><br><br><br><br><br>
 
 
     <div class="parallax" id="0.8" style="margin-top: 41%;z-index:6;">
-      <?php include 'modules/landingInfo.html' ?>
-    </div>
-    <div class="parallax" id="1.3">
-      <img src="images/BrachioFem.png" style="margin-left: 57%;margin-top: -20%;">
-    </div>
-
-    <br><br><br>
-    <div class="parallax" id="0.6">
       <?php include 'modules/landingInfo.html' ?>
     </div>
   </body>
