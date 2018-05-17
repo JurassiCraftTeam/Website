a2a7f51c39d59136b1362bc02947844071f996cf
SkyForge_ <goldr1000ftw@gmail.com>
2018-05-17 17:13:24 -0500

    Changed the DinoDex appearance
    Note this does not include mobile updates

--- pages/dinodex.php
+++ pages/dinodex.php
@@ -1,38 +1,44 @@
 <?php include 'modules/navbar.html'?>
-
 <div class="cardgroup">
   <div class="column">
     <div class="cardlist">
       <div class="card">
-        <h1>Category</h1>
+        <h1>Dinosarus</h1>
+        <p>Dinosaurs are the famous, prehistoric creatues that exist in the JurassiCraft mod. Best known, the T.Rex</p>
       </div>
 
       <div class="card">
-        <h1>Category</h1>
+        <h1>Mammals</h1>
+        <p>Mammals, creatures that have fur and give birth to live young. The most common of these is the Wolly Mamoth</p>
       </div>
 
       <div class="card">
-        <h1>Category</h1>
+        <h1>Reptiles/Anphibians</h1>
+        <p>anything that slithers, is covered in a slimy resin, or looks like a lizard, you'll find it here</p>
       </div>
     </div>
 
     <div class="cardlist">
       <div class="card">
-        <h1>Category</h1>
+        <h1>Birds</h1>
+        <p>Birds are flying creatures that typically are covered in feathers, their bones are hollow, and populate the skies</p>
       </div>
 
       <div class="card">
-        <h1>Category</h1>
+        <h1>Marine Life</h1>
+        <p>These creatures dwell in the deeps. If it breathes under water, or has a blowhole, you can expect to find it here.</p>
       </div>
 
       <div class="card">
-        <h1>Category</h1>
+        <h1>Misc</h1>
+        <p>This is where the weirdest of weird creatures go, "What even is that?"</p>
       </div>
     </div>
   </div>
   <div class="column">
     <div class="tallcard">
       <h1>Wiki</h1>
+      <p>The JurassiCraft wiki is where all the experts, or soon to be, go to get all the specific information on anything and everything in the mods. You can see how much health a Frog has, or how tall a Brachiosaurus is. These documentations will be updated frequently with new information almost every day!</p>
     </div>
   </div>
 </div>
