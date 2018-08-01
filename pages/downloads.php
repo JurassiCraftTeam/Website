fe35b70470368d4f62e93249370f6e2cdacad48c
Cazsius <cazsius0518@gmail.com>
2018-08-01 00:19:05 -0400

    Fixed Download Page

--- pages/downloads.php
+++ pages/downloads.php
@@ -2,23 +2,14 @@
 
 <br>
 
-<div class="background">
-  <button type="button" onclick="left();" style="width:50%;">JurassiCraft</button>
-  <button type="button" onclick="right();" style="width:50%;">Genesis</button>
-</div>
-
 <div class="pages">
 
   <div class="page" id="left">
-    <h1>JurassiCraft</h1>
-    <p>a bunch of info about jurassicraft will go here blah blah blah click this downloads page button:</p>
+	<img src="images/JC_Logo_Red.png" alt="JC Logo Red">
+	<font size="5">
+    <p> JurassiCraft is a mod based on Jurassic Park on Jurassic World franchises, but is not only limited to it.<br> 
+	This mod allows you to create dinosaurs by extracting DNA from fossils and amber, and putting that through a<br>
+	process until you get a baby dinosaur! Not only does this mod add dinosaurs, it also adds many prehistoric plants!</p>
     <button type="button" onclick="downloadJC();" style="width:90%;bottom: 10px;left: 5%;position: absolute;">JurassiCraft</button>
   </div>
-
-  <div class="page" id="right" style="left: 2000;">
-    <h1>Genesis</h1>
-    <p>a bunch of info about genesis will go here blah blah blah click this downloads page button:</p>
-    <button type="button" onclick="alert();" style="width:90%;bottom: 10px;left: 5%;position: absolute;">Genesis</button>
-  </div>
-
 </div>
--- pages/jurassicraft.php
+++ pages/jurassicraft.php
@@ -14,115 +14,63 @@
 
   <div class="release" id="recommended">
     <div style="display: flex;">
-      <div class="version" style="float: left;">1.12.2-1.1.0</div>
-      4 days ago
+      <div class="version" style="float: left;">2.1.14</div>
+      7/31/2018
       <div style="float: right;">
-        <button id="download">Download</button><br><br>
-        <button id="source">GitHub</button>
+        <button id="download" onclick="location.href='https://minecraft.curseforge.com/projects/jurassicraft/files/2595176/download'">Download</button><br><br>
+        <button id="source" onclick="location.href='https://github.com/JurassiCraftTeam/JurassiCraft2'">GitHub</button>
       </div>
     </div>
     <button onclick="viewContent('release');">Show Patch Notes</div>
     <div class="patchnotes" style="display: none;" id="release">
-      patch notes<br>
-      patch notes<br>
-      patch notes
+	  *Fixed some issues with the new lang utils<br>
+	  +The dinosaur order gui displays the current order now<br>
+	  *Krill to no longer have a collision box for entitites to stand on it
     </div>
   </div>
-</div>
+</div>	
 
 <div class="content">
   <h1>All Versions</h1>
   <div><div><div><div><div><div><div><div><div><div>
-    <div class="release" id="recommended">
+    <div class="release">
       <div style="display: flex;">
-        <div class="version" style="float: left;">1.12.2-1.1.0</div>
-        4 days ago
+        <div class="version" style="float: left;">2.1.13</div>
+        7/30/2018
         <div style="float: right;">
-          <button id="download">Download</button><br><br>
-          <button id="source">GitHub</button>
+          <button id="download" onclick="location.href='https://minecraft.curseforge.com/projects/jurassicraft/files/2594638/download'">Download</button><br><br>
+          <button id="source" onclick="location.href='https://github.com/JurassiCraftTeam/JurassiCraft2'">GitHub</button>
         </div>
       </div>
-      <button onclick="viewContent('1.1.0');">Show Patch Notes</div>
-      <div class="patchnotes" style="display: none;" id="1.1.0">
-        patch notes<br>
-        patch notes<br>
-        patch notes
+      <button onclick="viewContent('2.1.13');">Show Patch Notes</div>
+      <div class="patchnotes" style="display: none;" id="2.1.13">
+		*Fixed desyncing process bars for all machines<br>
+		*Fixed desyncing items when taken out of the output slots<br>
+		*Fixed client-side duplication bug with cleaning station<br>
+		*Fixed output slots accepting items from the player which isn't possible anymore<br>
+		*Fixed the feeders's default input slot -> Now upper left corner<br>
+		*Fixed a problem with herds losing their leader which crashed the game<br>
+		*Ensures dinosaurs do not remain angry after player leaves.<br>
+		*Improved some Dinosaur Targetting AI<br>
+		*Changed system to use LangUtils<br>
       </div>
     </div>
 
     <br>
-
+	
     <div class="release">
       <div style="display: flex;">
-        <div class="version" style="float: left;">1.12.2-1.0.3</div>
-        11 days ago
+        <div class="version" style="float: left;">2.1.12</div>
+        7/29/2018
         <div style="float: right;">
-          <button id="download">Download</button><br><br>
-          <button id="source">GitHub</button>
+          <button id="download" onclick="location.href='https://minecraft.curseforge.com/projects/jurassicraft/files/2591780/download'">Download</button><br><br>
+          <button id="source" onclick="location.href='https://github.com/JurassiCraftTeam/JurassiCraft2'">GitHub</button>
         </div>
       </div>
-      <button onclick="viewContent('1.0.3');">Show Patch Notes</div>
-      <div class="patchnotes" style="display: none;" id="1.0.3">
-        patch notes<br>
-        patch notes<br>
-        patch notes
-      </div>
-    </div>
-
-    <br>
-
-    <div class="release">
-      <div style="display: flex;">
-        <div class="version" style="float: left;">1.12.2-1.0.2</div>
-        15 days ago
-        <div style="float: right;">
-          <button id="download">Download</button><br><br>
-          <button id="source">GitHub</button>
-        </div>
-      </div>
-      <button onclick="viewContent('1.0.2');">Show Patch Notes</div>
-      <div class="patchnotes" style="display: none;" id="1.0.2">
-        patch notes<br>
-        patch notes<br>
-        patch notes
-      </div>
-    </div>
-
-    <br>
-
-    <div class="release">
-      <div style="display: flex;">
-        <div class="version" style="float: left;">1.12.2-1.0.1</div>
-        25 days ago
-        <div style="float: right;">
-          <button id="download">Download</button><br><br>
-          <button id="source">GitHub</button>
-        </div>
-      </div>
-      <button onclick="viewContent('1.0.1');">Show Patch Notes</div>
-      <div class="patchnotes" style="display: none;" id="1.0.1">
-        patch notes<br>
-        patch notes<br>
-        patch notes
-      </div>
-    </div>
-
-    <br>
-
-    <div class="release">
-      <div style="display: flex;">
-        <div class="version" style="float: left;">1.12.2-1.0.0</div>
-        30 days ago
-        <div style="float: right;">
-          <button id="download">Download</button><br><br>
-          <button id="source">GitHub</button>
-        </div>
-      </div>
-      <button onclick="viewContent('1.0.0');">Show Patch Notes</div>
-      <div class="patchnotes" style="display: none;" id="1.0.0">
-        patch notes<br>
-        patch notes<br>
-        patch notes
+      <button onclick="viewContent('2.1.12');">Show Patch Notes</div>
+      <div class="patchnotes" style="display: none;" id="2.1.12">
+		+Added a Config Option to allow Tour Rail blocks to be disabled<br>
+		*Fixed Action Figures not displaying the correct pose
       </div>
     </div>
 
